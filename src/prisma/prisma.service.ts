5f95a8605c018c64b5cb9963530e7909bff7437b
Lalit Rajak <lalitrajak312@gmail.com>
2025-05-30 18:41:55 +0530

    updated readme.md

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,10 @@ import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
